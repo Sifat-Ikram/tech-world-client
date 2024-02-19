77420b040825f2811f3b073ff0568942ebd27cbf
Sifat-Ikram <sifatikram@gmail.com>
2024-02-19 15:20:18 +0600

    website building started

---
+++ src/component/pages/errorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <button className="bg-[#0F1BB2] px-5 py-2 rounded-lg text-white font-bold">Back</button>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './component/pages/errorPage/ErrorPage.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <ErrorPage />,
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
