f0cd8bf2301225a50063dbffc610bbada7a30132
Sifat-Ikram <sifatikram@gmail.com>
2024-02-21 10:51:51 +0600

    client part updated

--- src/component/shared/footer/Footer.jsx
+++ src/component/shared/footer/Footer.jsx
@@ -3,7 +3,7 @@
 const Footer = () => {
     return (
         <div>
-            <footer className="footer footer-center mt-20 p-10 bg-[#0845F4] text-base-content rounded-none">
+            <footer className="footer footer-center mt-20 p-10 bg-[#0F1BB2] text-base-content rounded-none">
                 <nav className="grid grid-flow-col gap-4">
                     <a className="font-semibold text-white link link-hover" href='/about'>About us</a>
                     <a className="font-semibold text-white link link-hover" href='/contact'>Contact</a>
@@ -11,7 +11,7 @@ const Footer = () => {
                     <a className="font-semibold text-white link link-hover">Press kit</a>
                 </nav>
                 <div className="block text-white cursor-pointer font-lato" href='/'>
-                    <h1 className='text-4xl font-extrabold uppercase'>Tech World</h1>
+                    <h1 className='md:text-4xl text-xl font-extrabold uppercase'>Tech World</h1>
                 </div>
                 <aside className='text-white'>
                     <p>Copyright © 2023 - All right reserved by Tech World Ltd</p>
